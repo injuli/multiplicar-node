f7225ef2800d68724d099987544999373da5edcd
ingrid <ingrijrm94@gmail.com>
2019-06-12 17:25:38 -0500

    agregó readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+## Node Multiplicar console App
+
+Esta es una aplicación que genera archivos de tablas de multiplicar.
+
+Ejecutar este comando:
+
+```
+npm install
+```
--- config/yargs.js
+++ config/yargs.js
@@ -10,8 +10,8 @@ const opts = {
 }
 
 const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de múltiplicar', opts)
-    .command('crear', 'Genera un archivo con la tabla de múltiplicar.', opts)
+    .command('listar', 'Imprime en consola la tabla de multiplicar', opts)
+    .command('crear', 'Genera un archivo con la tabla de multiplicar.', opts)
     .help()
     .argv;
 
